28db4ed9de4bfdb13e1c660e593a17575295a98c
uber5001 <uber5001@gmail.com>
2015-07-08 16:36:50 -0700

    WIP: make travis logs and result useful

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,6 +1,8 @@
 ./scripts/build_app.sh
-./node_modules/appium/bin/appium.js &
+./node_modules/appium/bin/appium.js --log-level info &
 APPIUM_PID=$!
 sleep 10
 ./node_modules/jasmine/bin/jasmine.js
+EXIT_STATUS=$?
 kill $APPIUM_PID
+exit $EXIT_STATUS

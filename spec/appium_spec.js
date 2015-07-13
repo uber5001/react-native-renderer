760c52983f17342432024c07168990606a6d5827
uber5001 <uber5001@gmail.com>
2015-07-13 15:10:06 -0700

    WIP: use Travis to test on Sauce

--- spec/appium_spec.js
+++ spec/appium_spec.js
@@ -3,20 +3,26 @@
 var wd = require("wd");
 var path = require("path");
 // var bundler = require("./bundle");
-
+var travis = process.env.TRAVIS;
 var desired = {
 	browserName: '',
 	appiumVersion: "1.4.7",
 	deviceName: "iPhone Simulator",
 	deviceOrientation: "portrait",
 	// platformVersion: "8.3",
-	platformVersion: "8.2",
+	// platformVersion: "8.2",
 	platformName: "iOS",
 	// app: path.resolve("dist/build/dist.app.zip"),
-	app: "sauce-storage:dist.app.zip",
+	// app: "sauce-storage:dist2.app.zip",
 	launchTimeout: 600000
 };
-// desired = {};
+if (travis) {
+	desired.app = "sauce-storage:" + process.env.SAUCE_APP_NAME
+	desired.platformVersion = "8.2"
+} else {
+	desired.app = path.resolve("dist/build/dist.app.zip")
+	desired.platformVersion = "8.3"
+}
 //appium also closes a session after 60s, no reason to wait longer than that. (capability newCommandTimeout)
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 var APPIUM_INIT_TIMEOUT = 1200000;
@@ -26,9 +32,11 @@ describe('todo-app', function() {
 	var catchSpy;
 	beforeEach(function(done) {
 		catchSpy = jasmine.createSpy("catchSpy");
-
-		browser = wd.promiseChainRemote("http://angular-ci:secret-sauce@ondemand.saucelabs.com/wd/hub", 80);
-		// browser = wd.promiseChainRemote("0.0.0.0", 4723);
+		if (travis) {
+			browser = wd.promiseChainRemote("http://angular-ci:" + process.env.SAUCE_ACCESS_KEY + "@ondemand.saucelabs.com/wd/hub", 80);
+		} else {
+			browser = wd.promiseChainRemote("0.0.0.0", 4723);
+		}
 		browser.on('status', function(info) {
 		  console.log('status\n', arguments);
 		});
